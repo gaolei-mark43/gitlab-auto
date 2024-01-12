523b957c9f57bbc626e639de2708a82303891ec5
gaolei-mark43 <656634739@qq.com>
2024-01-12 09:20:04 +0800

    update a shell or config file

--- GiTee/config/config.py
+++ GiTee/config/config.py
@@ -1,5 +1,5 @@
 # api地址和token的加密解密
-baseUrl = "https://code.iflytek.com/api/gitlab"
-key = b'tLXXOjx_kpQz1ttTEr-kjYDiX6c7UEDI70LflyjKFUk='
-encrypted_token = 'gAAAAABkDUikhextfcshdDX9JykkyEoSQ3Hvzrxq4ZBQ0aO9eWdGPaH1lMPrLJgAKH_66rtmm-au_reI' \
+baseUrl = "https://code.gitlab.com/api/gitlab"
+key = b'tLXXXXXx_kpQtTEr-kjYDiX6c7UEDI70LflyjKFUk='
+encrypted_token = 'gAAAAABkDUiXXXXXXXXXXXXq4ZBQ0aO9eWdGPaH1lMPrLJgAKH_66rtmm-au_reI' \
                   '-WAjVfmgBTvOgkGN99jznHFoJcQZsLIfmUXohnK0p5okVjndOnT5hLdWi9L1'
--- GiTee/start.sh
+++ GiTee/start.sh
@@ -4,6 +4,6 @@
 # 解开main.py第三行、第四行的注释
 sed -i '3,4s/^# //' main.py
 # 211行IP替换为Linux服务器IP
-sed -i 's/127.0.0.1/172.30.94.147/g' main.py
+sed -i 's/127.0.0.1/101.10.10.101/g' main.py
 # 后台运行，输出到日志文件中
 nohup python3 -u main.py > log`date +%s`.log 2>&1 &
